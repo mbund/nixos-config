5cf32fe5408a426c9c0e814d87a074a50bc04271
mbund <bundschuhmark@gmail.com>
2021-12-19 23:30:08 -0500

    cleanup

--- systems/virtualbox/flake.nix
+++ systems/virtualbox/flake.nix
@@ -7,7 +7,60 @@
   {
     nixosConfigurations.virtualbox = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ({ pkgs, ... }: {
+          
+          imports = [
+            ./hardware-configuration.nix
+          ];
+
+          nix = {
+            extraOptions = ''
+              # enable the new standalone nix commands
+              experimental-features = nix-command flakes
+
+              # allow rebuild while offline
+              keep-outputs = true
+              keep-derivations = true
+            '';
+          };
+
+          networking.hostName = "virtualbox";
+          time.timeZone = "America/New_York";
+
+          users.users = {
+            mbund = {
+              isNormalUser = true;
+              extraGroups = [ "wheel" ];
+              uid = 1000;
+              initialPassword = "mbund";
+            };
+          };
+
+          services.xserver = {
+            enable = true;
+            displayManager.sddm.enable = true;
+            desktopManager.plasma5.enable = true;
+          };
+
+          services.pipewire = {
+            enable = true;
+            alsa = {
+              enable = true;
+              support32Bit = true; # this is probably not necessary
+            };
+            pulse.enable = true;
+          };
+
+          boot.loader.grub = {
+            enable = true;
+            version = 2;
+            device = "/dev/sda";
+          };
+
+          system.stateVersion = "21.11";
+        })
+      ];
     };
   };
 }
