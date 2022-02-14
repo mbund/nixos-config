543d70450083d2579fafa4a1d0aec85c1b3af17c
mbund <bundschuhmark@gmail.com>
2022-02-14 17:07:10 -0500

    remove nixos-installer profile

--- flake.nix
+++ flake.nix
@@ -5,15 +5,13 @@
     virtualbox.url = "./virtualbox";
     marshmellow-roaster.url = "./marshmellow-roaster";
     desktop.url = "./desktop";
-    nixos-installer.url = "./nixos-installer";
   };
 
   outputs = { self, ... }@inputs: with inputs; {
     nixosConfigurations =
       virtualbox.nixosConfigurations //
       marshmellow-roaster.nixosConfigurations //
-      desktop.nixosConfigurations //
-      nixos-installer.nixosConfigurations;
+      desktop.nixosConfigurations;
 
   };
 }
