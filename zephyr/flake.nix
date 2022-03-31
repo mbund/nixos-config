8f3d5f96190eacd3ed6b1c428138f98434bd28e3
mbund <25110595+mbund@users.noreply.github.com>
2022-03-30 21:44:15 -0400

    remove seahorse

--- zephyr/flake.nix
+++ zephyr/flake.nix
@@ -145,7 +145,6 @@
 
               # Keychain options
               services.gnome.gnome-keyring.enable = true;
-              programs.seahorse.enable = true;
               security.pam.services.login = {
                 enableGnomeKeyring = true;
               };
