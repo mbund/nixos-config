c8def25a8a03d360e03eb022372c4de09729eb79
mbund <25110595+mbund@users.noreply.github.com>
2022-04-28 21:31:53 -0400

    work on custom desktop environment

--- marshmellow-roaster/de/flake.nix
+++ marshmellow-roaster/de/flake.nix
@@ -2,63 +2,46 @@
   description = "System configurations for custom desktop environment";
 
   outputs = { self, ... }@inputs: {
-    nixosModule = { pkgs, config, lib, ... }: let
-      cfg = config.services.custom-desktop-environment;
-    in {
+    nixosModule = { pkgs, config, lib, ... }:
+      let
+        cfg = config.services.custom-desktop-environment;
+      in
+      {
 
-      options.services.custom-desktop-environment = {
-        enable = lib.mkOption {
-          default = false;
-          type = lib.types.bool;
-          description = ''
-            Whether or not to enable the custom desktop environment.
-          '';
-        };
-
-        login-manager = {
+        options.services.custom-desktop-environment = {
           enable = lib.mkOption {
             default = false;
             type = lib.types.bool;
             description = ''
-              Whether or not to enable the recommended login manager for the custom desktop environment.
+              Whether or not to enable the custom desktop environment.
             '';
           };
-
-          default-user = lib.mkOption {
-            example = "mbund";
-            type = lib.types.str;
-            description = ''
-              Username for the default session to log in to.
-            '';
-          };
-
-        };
-      };
-
-      config = lib.mkIf cfg.enable {
-
-        xdg.portal = {
-          enable = true;
-          wlr = {
-            enable = true;
-          };
-          gtkUsePortal = true;
         };
 
-        hardware.opengl.enable = true;
-        programs.xwayland.enable = true;
-        programs.dconf.enable = true;
-        services.pipewire = {
-          enable = true;
-          alsa = {
+        config = lib.mkIf cfg.enable {
+
+          xdg.portal = {
             enable = true;
-            support32Bit = true; # this is probably not necessary
+            wlr = {
+              enable = true;
+            };
+            gtkUsePortal = true;
           };
-          pulse.enable = true;
+
+          hardware.opengl.enable = true;
+          programs.xwayland.enable = true;
+          programs.dconf.enable = true;
+          services.pipewire = {
+            enable = true;
+            alsa = {
+              enable = true;
+              support32Bit = true; # this is probably not necessary
+            };
+            pulse.enable = true;
+          };
+
         };
 
       };
-
-    };
   };
 }
